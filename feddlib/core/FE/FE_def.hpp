469d52883c715caa4da23573ce6291281d102a94
Lea <lea.sassmannshausen@hotmail.de>
2025-10-16 18:21:00 +0200

    Update absorbing unsteady

--- feddlib/core/FE/FE_def.hpp
+++ feddlib/core/FE/FE_def.hpp
@@ -5109,7 +5109,7 @@ double FE<SC,LO,GO,NO>::assemblyAbsorbingBoundaryPaper(int dim,
     if(funcParameter[0] < unsteadyStart)
         h_x=  std::pow( (std::sqrt(density)/(2*std::sqrt(2)) * flowRateInlet/A_bar + std::sqrt(beta*std::sqrt(areaOutlet_init))),2) - beta*std::sqrt(areaOutlet_init); // In the so called steady phase we also try to use the inlet flowrate
     else
-        h_x=  std::pow( (std::sqrt(density)/(2*std::sqrt(2)) * flowRateUse/areaOutlet + std::sqrt(beta*std::sqrt(areaOutlet_T))),2) - beta*std::sqrt(areaOutlet_T) + p_ref;
+        h_x=  std::pow( (std::sqrt(density)/(2*std::sqrt(2)) * flowRateInlet/areaOutlet + std::sqrt(beta*std::sqrt(areaOutlet_T))),2) - beta*std::sqrt(areaOutlet_T) + p_ref;
 
     //         h_x=  pow( (sqrt(density)/(2*sqrt(2)) * flowRateUse/areaOutlet + sqrt(beta*sqrt(areaOutlet_init))),2) - beta*sqrt(areaOutlet_init) + p_ref;
 
