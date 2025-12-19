728f8c02fc31450a9f0e40d23796c190d94d5718
Lea <lea.sassmannshausen@hotmail.de>
2025-12-19 10:56:14 +0100

    Add newton reuse to fsci class by setting newtonStep_=0 when we update time

--- feddlib/problems/specific/FSCI_def.hpp
+++ feddlib/problems/specific/FSCI_def.hpp
@@ -541,6 +541,7 @@ void FSCI<SC,LO,GO,NO>::reAssemble(std::string type) const
 template<class SC,class LO,class GO,class NO>
 void FSCI<SC,LO,GO,NO>::updateTime() const
 {
+    this->newtonStep_ = 0;
     this->timeSteppingTool_->t_ = this->timeSteppingTool_->t_ + this->timeSteppingTool_->dt_prev_;
     this->problemTimeFluid_->updateTime(this->timeSteppingTool_->t_);
     this->problemSCI_->reAssemble("UpdateTime");
