7598f78212b03e1f51a67a86f26005add9a29db6
Lea <lea.sassmannshausen@hotmail.de>
2026-01-14 14:48:17 +0100

    Improve sparsity allocation for establishing nnz pattern function

--- feddlib/problems/specific/NavierStokes_def.hpp
+++ feddlib/problems/specific/NavierStokes_def.hpp
@@ -396,8 +396,11 @@ void NavierStokes<SC,LO,GO,NO>::assembleConstantMatrices() const{
     if(nnzPatternEstablished_==false)
         establishNNZPattern();
 
-    
-    MatrixPtr_Type A_withNNZ = Teuchos::rcp( new Matrix_Type( this->getDomain(0)->getMapVecFieldUnique(), this->getDomain(0)->getDimension() * this->getDomain(0)->getApproxEntriesPerRow() ) );
+    int allocationFactor = 1;
+    if(augmentedLagrange_)
+        allocationFactor = 3;
+
+    MatrixPtr_Type A_withNNZ = Teuchos::rcp( new Matrix_Type( this->getDomain(0)->getMapVecFieldUnique(), allocationFactor*this->getDomain(0)->getDimension() * this->getDomain(0)->getApproxEntriesPerRow() ) );
     A_->addMatrix(1.,A_withNNZ,0.);
     NNZ_A_->addMatrix(1.,A_withNNZ,1.);
 
