a462889d04f71524496ee42d9f3267199b7789e5
Lea <lea.sassmannshausen@hotmail.de>
2026-01-14 14:44:18 +0100

    remove paraview export

--- feddlib/problems/examples/steadyNavierStokes/main.cpp
+++ feddlib/problems/examples/steadyNavierStokes/main.cpp
@@ -356,7 +356,7 @@ int main(int argc, char *argv[]) {
                 }
                 domainVelocity->preProcessMesh(true,false);
 
-                domainVelocity->exportNodeFlags("Fluid");
+                // domainVelocity->exportNodeFlags("Fluid");
                 
                 // domainVelocity->exportMesh(false,false,"FluidMesh");
 
