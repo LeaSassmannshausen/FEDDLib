fcda49d607b4d8226c2c87391a1f93f11527e93d
Lea <lea.sassmannshausen@hotmail.de>
2026-01-28 16:33:40 +0100

    remove error

--- feddlib/problems/tests/steadyNonLinElasAssFE/main.cpp
+++ feddlib/problems/tests/steadyNonLinElasAssFE/main.cpp
@@ -355,29 +355,30 @@ int main(int argc, char *argv[])
 
 		}    
        
+        MultiVectorConstPtr_Type valuesSolidConst1 = NonLinElas.getSolution()->getBlock(0);
+        MultiVectorConstPtr_Type valuesSolidConst2 = NonLinElasAssFE.getSolution()->getBlock(0);
+        // Calculating the error per node
+        Teuchos::RCP<MultiVector<SC,LO,GO,NO> > errorValues = Teuchos::rcp(new MultiVector<SC,LO,GO,NO>( valuesSolidConst1->getMap() ) ); 
+        //this = alpha*A + beta*B + gamma*this
+        errorValues->update( 1., valuesSolidConst2, -1. ,valuesSolidConst1, 0.);
+        // Taking abs norm
+        Teuchos::RCP<const MultiVector<SC,LO,GO,NO> > errorValuesAbs = errorValues;
+
+        errorValues->abs(errorValuesAbs);
+
         if( parameterListProblem->sublist("General").get("ParaViewExport",false) ) {
+
             Teuchos::RCP<ExporterParaView<SC,LO,GO,NO> > exPara(new ExporterParaView<SC,LO,GO,NO>());
 
             exPara->setup( "displacements", domain->getMesh(), FEType );
 
-            MultiVectorConstPtr_Type valuesSolidConst1 = NonLinElas.getSolution()->getBlock(0);
             exPara->addVariable( valuesSolidConst1, "valuesNonLinElas", "Vector", dim, domain->getMapUnique());
 
-            MultiVectorConstPtr_Type valuesSolidConst2 = NonLinElasAssFE.getSolution()->getBlock(0);
             exPara->addVariable( valuesSolidConst2, "valuesNonLinElasAssFE", "Vector", dim, domain->getMapUnique());
-
-            // Calculating the error per node
-            Teuchos::RCP<MultiVector<SC,LO,GO,NO> > errorValues = Teuchos::rcp(new MultiVector<SC,LO,GO,NO>( valuesSolidConst1->getMap() ) ); 
-            //this = alpha*A + beta*B + gamma*this
-            errorValues->update( 1., valuesSolidConst2, -1. ,valuesSolidConst1, 0.);
-
-            // Taking abs norm
-            Teuchos::RCP<const MultiVector<SC,LO,GO,NO> > errorValuesAbs = errorValues;
-
-            errorValues->abs(errorValuesAbs);
-
+        
             exPara->addVariable( errorValuesAbs, "erroeValues", "Vector", dim, domain->getMapUnique());
             exPara->save(0.0);
+            
         }
 
         Teuchos::Array<SC> norm(1); 
