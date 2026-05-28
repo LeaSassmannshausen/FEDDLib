b8278a766431d325abdb0265d3266dc0a1e4c1f0
Lea <lea.sassmannshausen@hotmail.de>
2026-05-28 10:17:41 +0200

    Rest RB

--- feddlib/problems/examples/sci/main.cpp
+++ feddlib/problems/examples/sci/main.cpp
@@ -261,7 +261,7 @@ void rhsHeartBeatArtery(double* x, double* res, double* parameters){
     double Q = 0.5*a0;
     
 
-    double t_min = t - fmod(t,1.0)+heartBeatStart+0.5-std::floor(t)+0.02; //FlowConditions::t_start_unsteady;
+    double t_min = t - fmod(t,1.0)+heartBeatStart+0.5-std::floor(t)-0.02; //FlowConditions::t_start_unsteady;
     double t_max = t_min + 0.5; // One heartbeat lasts 0.5 seconds    
     double y = M_PI * ( 2.0*( t-t_min ) / ( t_max - t_min ) -1.0)  ;
     
