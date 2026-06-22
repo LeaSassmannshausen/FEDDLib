c99b76d215036d8173216b45f81671cf8b24adf8
Lea <lea.sassmannshausen@hotmail.de>
2026-06-23 00:00:39 +0200

    Update varying timesteps

--- feddlib/problems/specific/FSCI_def.hpp
+++ feddlib/problems/specific/FSCI_def.hpp
@@ -171,7 +171,7 @@ void FSCI<SC,LO,GO,NO>::assemble( std::string type ) const
         // C2*d_s^n
         // this->C2_ = C2;
         this->C2_unscaled_ = Teuchos::rcp(new Matrix_Type(C2));
-        this->C2_ = Teuchos::rcp(new Matrix_Type(C2_unscaled_));
+        this->C2_ = Teuchos::rcp(new Matrix_Type(this->C2_unscaled_));
         this->C2_->scale( -(1.0/dt) ); // th
         
         if(!this->geometryExplicit_)
